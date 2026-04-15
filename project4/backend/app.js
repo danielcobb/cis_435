aacd241a51ca825221bf893b97d8d2229bff8323
danielcobb <danielcobb20@gmail.com>
2026-04-15 17:42:42 -0400

    finished the backend for now

--- project4/backend/app.js
+++ project4/backend/app.js
@@ -1,11 +1,21 @@
-import express from 'express';
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
-const app = express();
+dotenv.config()
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+const app = express()
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+connectDB()
+
+app.use(cors())
+app.use(express.json())
+
+//routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/listings', require('./routes/listing'))
+
+app.listen(process.env.port, () => {
+    console.log(`Server running on port ${process.env.PORT}`)
+})
--- project4/backend/middleware/authMiddleware.js
+++ project4/backend/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
-import jwt from 'jsonwebtoken'
-const User = require ('./models/user')
+const jwt = require('jsonwebtoken')
+const User = require ('../models/User')
 
 const protect = async (req, res, next) => {
     let token
