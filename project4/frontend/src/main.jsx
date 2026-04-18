128f94e547063d2feef934c4aea9e98db941e979
danielcobb <danielcobb20@gmail.com>
2026-04-18 16:13:35 -0400

    working on styling

--- project4/frontend/src/main.jsx
+++ project4/frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { AuthProvider } from "./context/AuthContext";
+import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
